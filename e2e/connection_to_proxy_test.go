b1a3dd806a3aa82fa370108299a46780aaf1351a
Martin-Alexander <martingianna@gmail.com>
2025-05-20 04:46:37 -0400

    rename e2e test

--- e2e/connection_to_proxy_test.go
+++ e2e/connection_to_proxy_test.go
@@ -12,7 +12,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func TestConnectionToProxyServer(t *testing.T) {
+func TestConnectionToServer(t *testing.T) {
 	id := rand.Int()
 
 	databaseName := fmt.Sprintf("test_db_%d", id)
